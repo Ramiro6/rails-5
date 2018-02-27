c333980828669f995e7f59d165e1610802f1a0af
Ramiro Ruiz Larrabure <ramiro.ruiz@tecnotramit.com>
2018-02-27 15:58:08 +0100

    add controller

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,15 @@
+class DemoController < ApplicationController
+  layout false
+  def index
+    render('index')
+  end
+
+  def hello
+    render('hello')
+  end
+
+  def other_hello
+    redirect_to action: 'index'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'demo/index'
+  get 'demo/hello'
+  get 'demo/other_hello'
+  root 'demo#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
