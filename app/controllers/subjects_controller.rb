b4866e0bcf57e7b4d5bcda93c82b736295122596
Ramiro <ramiroruiz59@gmail.com>
2018-03-01 12:12:01 +0100

    add create event

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,14 +1,23 @@
 class SubjectsController < ApplicationController
   def index
+    @subjects = Subject.sorted
   end
 
   def show
+    @subjects = Subject.find(params[:id])
   end
 
   def new
+    @subject = Subject.new({ name: 'default' })
   end
 
   def create
+    @subejct = Subject.new(subject_params)
+    if @subejct.save
+      redirect_to subjects_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -21,5 +30,10 @@ class SubjectsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get 'subjects/index'
-  get 'subjects/show'
-  get 'subjects/new'
-  get 'subjects/edit'
-  get 'subjects/delete'
+  root 'demo#index'
 
-  
+  resources :subjects do 
+    member do 
+      get :delete
+    end 
+  end
+
+
+
   get 'demo/index'
   get 'demo/hello'
   get 'demo/other_hello'
-  root 'demo#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
